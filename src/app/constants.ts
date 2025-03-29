b8853729e9e535c0a3daf8f5f4c4fbbe94e7922c
kianlejano <llejano@ccci-tech.com>
2025-03-29 16:07:36 +0800

    homepage

---
+++ src/app/constants.ts
@@ -0,0 +1,27 @@
+export const PERSONAL_INFO = {
+    firstName: 'Lance Kian',
+    nickname: 'Kian',
+    lastName: 'Lejano',
+    fullName: 'Lance Kian Calayag Lejano',
+    position: 'Full Stack Developer',
+    intro: 'I build fast, scalable, and user-friendly web applications using different frameworks. I am passionate about solving problems and crafting seamless digital experiences.'
+}
+
+export const NAV_MENU = [
+    { name: "Home", path: "/" },
+    { name: "Projects", path: "/projects" },
+    { name: "Skills", path: "/skills" },
+    { name: "Contact", path: "/contact" },
+  ];
+  
+  export const SOCIAL_LINKS = {
+    github: "https://github.com/kianlejano",
+    linkedin: "https://www.linkedin.com/in/lance-kian-lejano-a10029345/",
+  };
+  
+  export const SITE_METADATA = {
+    appName: 'Kiyan',
+    title: "My Portfolio",
+    description: "A showcase of my web development projects.",
+  };
+
